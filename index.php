6f27d495d6c38d073ac80bfd9dd3bca9ba3b1af1
webkam <k.white@highpointwebtech.com>
2015-12-20 12:49:21 -0500

    installed bootstrap template

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyle.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  </body>
+</html>
